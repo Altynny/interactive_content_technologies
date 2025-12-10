91f8633bc8c5a7e75b4023ed934deb3747716069
Ilya <altynnikovila@gmail.com>
2025-12-10 19:55:24 +0800

    Move errors to corresponding blocks

--- LaravelStart/resources/views/form.blade.php
+++ LaravelStart/resources/views/form.blade.php
@@ -9,31 +9,31 @@
         </div>
     @endif
 
-    @if($errors->any())
-        <div class="errors">
-            <ul style="margin:0; padding-left:18px;">
-                @foreach($errors->all() as $error)
-                    <li>{{ $error }}</li>
-                @endforeach
-            </ul>
-        </div>
-    @endif
     <form method="POST" action="{{ route('form.submit') }}">
         @csrf
 
         <div style="margin-bottom:10px;">
             <label for="name">Имя пользователя</label><br>
             <input id="name" name="name" value="{{ old('name') }}" style="width:100%; padding:8px;">
+            @error('name')
+            <div class="errors">{{ $message }}</div>
+            @enderror
         </div>
 
         <div style="margin-bottom:10px;">
             <label for="email">Email</label><br>
             <input id="email" name="email" value="{{ old('email') }}" style="width:100%; padding:8px;">
+            @error('email')
+            <div class="errors">{{ $message }}</div>
+            @enderror
         </div>
 
         <div style="margin-bottom:10px;">
             <label for="message">Сообщение</label><br>
             <textarea id="message" name="message" rows="6" style="width:100%; padding:8px;">{{ old('message') }}</textarea>
+            @error('message')
+            <div class="errors">{{ $message }}</div>
+            @enderror
         </div>
 
         <button type="submit" style="padding:10px 14px;">Отправить</button>
